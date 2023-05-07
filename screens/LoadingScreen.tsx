e7e9a0ff386e5a59407fd74da346fb964b8513a8
Tornike Jandieri <tornikejandieri7@gmail.com>
2023-05-07 20:00:26 +0400

    emoji animation

--- screens/LoadingScreen.tsx
+++ screens/LoadingScreen.tsx
@@ -1,10 +1,33 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { View, Animated, Text, StyleSheet, Image  } from 'react-native'
+
+const emoji = ['😊', '😢', '🤬', '😨', '🤩', '😌', '😖', '😐', '😑']
+
+
+const getRandomEmojis = () => {
+  const randomIndices: any = []
+  while (randomIndices.length < 3) {
+    const randomIndex = Math.floor(Math.random() * emoji.length)
+    if (!randomIndices.includes(randomIndex)) {
+      randomIndices.push(randomIndex)
+    }
+  }
+  return randomIndices.map((index: string | number) => emoji[index as any] )
+}
+
 
 const LoadingScreen = () => {
   const opacityValue = useRef(new Animated.Value(0)).current
   const scaleValue = useRef(new Animated.Value(0)).current
   const bgOpacityValue = useRef(new Animated.Value(0)).current
+  const [emojis, setEmojis] = useState(getRandomEmojis())
+
+  useEffect(() => {
+   const interval = setInterval(() => setEmojis(getRandomEmojis()), 500)
+
+    return () => clearInterval(interval)
+  }, [])
+
 
   useEffect(() => {
     Animated.sequence([
@@ -35,6 +58,9 @@ const LoadingScreen = () => {
     <View style={styles.container}>
       <Animated.Image source={require('../assets/khasiati-app-logo.png')} style={[styles.logoContainer, { opacity: opacityValue, transform: [{ scale: scaleValue }] }]} />
       <Animated.Image source={require('../assets/rainbow.png')} style={[styles.backgroundImage, { opacity: bgOpacityValue }]} />
+      <View style={{display: 'flex', flexDirection: 'row', position: 'absolute', bottom: '20%'}}>
+        <Text style={{fontSize: 50}}>{emojis}</Text>
+      </View>
     </View>
   );
 }
