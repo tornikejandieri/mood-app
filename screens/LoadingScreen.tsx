25f7d16f8ce96b929a17ea5490c2aef16c900fc7
Tornike Jandieri <tornikejandieri7@gmail.com>
2023-05-08 22:25:28 +0400

    loading animation changes

--- screens/LoadingScreen.tsx
+++ screens/LoadingScreen.tsx
@@ -6,6 +6,7 @@ import { useDispatch, useSelector } from "react-redux"
 import { RootState } from "../redux/store"
 import AsyncStorage from "@react-native-async-storage/async-storage"
 import { setThemeByValue } from "../models/themereducer/themeReducer"
+import useRenderRef from "../custom hooks/useRenderRef"
 
 const getRandomEmojis = () => {
   const randomIndices: any = []
@@ -19,15 +20,12 @@ const getRandomEmojis = () => {
 }
 
 const LoadingScreen = (props: { navigation: any }) => {
-  const opacityValue = useRef(new Animated.Value(0)).current
-  const scaleValue = useRef(new Animated.Value(0)).current
-  const bgOpacityValue = useRef(new Animated.Value(0)).current
   const [emojis, setEmojis] = useState(getRandomEmojis())
 
   const theme = useSelector((state: RootState) => state.theme.value)
   const dispatch = useDispatch()
 
-  setTimeout(() => props.navigation.navigate("Drawer"), 5000)
+  setTimeout(() => props.navigation.navigate("Drawer"), 4000)
 
   const isFocused = useIsFocused()
 
@@ -45,60 +43,15 @@ const LoadingScreen = (props: { navigation: any }) => {
   useEffect(() => {
     let interval: string | number | NodeJS.Timeout | undefined
     if (isFocused) {
-      interval = setInterval(() => setEmojis(getRandomEmojis()), 500)
+      interval = setInterval(() => setEmojis(getRandomEmojis()), 300)
     }
 
     return () => clearInterval(interval)
   }, [isFocused])
 
-  useEffect(() => {
-    Animated.sequence([
-      Animated.timing(bgOpacityValue, {
-        toValue: 1,
-        duration: 1000,
-        useNativeDriver: true,
-      }),
-      Animated.timing(opacityValue, {
-        toValue: 1,
-        duration: 1500,
-        useNativeDriver: true,
-      }),
-      Animated.timing(scaleValue, {
-        toValue: 1,
-        duration: 300,
-        useNativeDriver: true,
-      }),
-      Animated.spring(scaleValue, {
-        toValue: 0.3,
-        friction: 2,
-        useNativeDriver: true,
-      }),
-    ]).start()
-  }, [])
-
   return (
     <View style={styles.container}>
-      <Animated.Image
-        source={require("../assets/khasiati-app-logo.png")}
-        style={[
-          styles.logoContainer,
-          { opacity: opacityValue, transform: [{ scale: scaleValue }] },
-        ]}
-      />
-      <Animated.Image
-        source={require("../assets/rainbow.png")}
-        style={[styles.backgroundImage, { opacity: bgOpacityValue }]}
-      />
-      <View
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          position: "absolute",
-          bottom: "20%",
-        }}
-      >
-        <Text style={{ fontSize: 50 }}>{emojis}</Text>
-      </View>
+      <Text style={{ fontSize: 50 }}>{emojis}</Text>
     </View>
   )
 }
@@ -123,7 +76,7 @@ const styles = StyleSheet.create({
   },
   backgroundImage: {
     position: "absolute",
-    width: "100%",
-    height: "100%",
+    width: 200,
+    height: 200,
   },
 })
