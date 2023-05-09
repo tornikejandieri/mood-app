52a45f13934e8e13a6a810c307dd3007ce856aad
Tornike Jandieri <tornikejandieri7@gmail.com>
2023-05-09 13:42:38 +0400

    added app icon, removed statubar globally

--- App.tsx
+++ App.tsx
@@ -2,10 +2,12 @@ import StackNavigator from "./navigator/AppNavigator"
 import "react-native-gesture-handler"
 import { store } from "./redux/store"
 import { Provider } from "react-redux"
+import CustomStatusBar from "./components/CustomStatusBar"
 
 export default function App() {
   return (
     <Provider store={store}>
+      <CustomStatusBar />
       <StackNavigator />
     </Provider>
   )
---
+++ components/CustomStatusBar.tsx
@@ -0,0 +1,8 @@
+import { StatusBar } from "react-native"
+import React from "react"
+
+const CustomStatusBar = () => {
+  return <StatusBar hidden={true} />
+}
+
+export default CustomStatusBar
