e65f649a43039f1f12191cb385b96ce494d7b5e0
Tornike Jandieri <tornikejandieri7@gmail.com>
2023-05-10 22:50:06 +0400

    grid and list view

--- screens/StatisticsScreen.tsx
+++ screens/StatisticsScreen.tsx
@@ -1,17 +1,16 @@
 import {
-  Alert,
-  Animated,
   FlatList,
   Text,
   TouchableOpacity,
   View,
+  useWindowDimensions,
 } from "react-native"
 import React, { useCallback, useEffect, useRef, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { RootState } from "../redux/store"
 import AsyncStorage from "@react-native-async-storage/async-storage"
 import { colors } from "../constants/colors"
-import { dateFormatter, minutesHoursAndDays } from "../helpers"
+import { minutesHoursAndDays } from "../helpers"
 import { useIsFocused } from "@react-navigation/native"
 import useRenderRef from "../custom hooks/useRenderRef"
 import HalfScreenModal from "../components/HalfScreenModal"
@@ -19,6 +18,8 @@ import SlideButton from "../components/SlideButton"
 import { toggleTheme } from "../models/themereducer/themeReducer"
 import { Entypo } from "@expo/vector-icons"
 import Notification from "../components/Notification"
+
+import { TabView } from "react-native-tab-view"
 
 const StatisticsScreen = () => {
   const [showTimeModal, setShowTimeModal] = useState(false)
@@ -31,7 +32,13 @@ const StatisticsScreen = () => {
 
   const isFocused = useIsFocused()
 
-  useRenderRef()
+  const layout = useWindowDimensions()
+
+  const [index, setIndex] = useState(0)
+  const [routes] = useState([
+    { key: "first", title: "First" },
+    { key: "second", title: "Second" },
+  ])
 
   useEffect(() => {
     const getData = async () => {
@@ -97,6 +104,86 @@ const StatisticsScreen = () => {
     )
   }
 
+  const renderGridItem = ({
+    item,
+  }: {
+    item: { mood: string; date: string }
+  }) => {
+    return (
+      <TouchableOpacity
+        activeOpacity={0.6}
+        style={{
+          height: 150,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          borderWidth: 0.5,
+          marginHorizontal: 10,
+          marginVertical: 5,
+          borderColor: colors.disabled,
+          borderRadius: 10,
+          flex: 1,
+        }}
+        onPress={() => {
+          setShowTimeModal(true)
+          setSelectedDate(item.date)
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 25,
+            padding: 10,
+            color: theme === "dark" ? colors.disabled : colors.black,
+          }}
+        >
+          {item.mood}
+        </Text>
+        <Text
+          style={{
+            fontSize: 16,
+            color: theme === "dark" ? colors.disabled : colors.black,
+          }}
+        >
+          {`${minutesHoursAndDays(item.date)} ago`}
+        </Text>
+      </TouchableOpacity>
+    )
+  }
+
+  const renderScene = ({ route }) => {
+    switch (route.key) {
+      case "first":
+        return (
+          <FlatList
+            data={data}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.date}
+            showsVerticalScrollIndicator={false}
+          />
+        )
+      case "second":
+        return (
+          <FlatList
+            data={data}
+            renderItem={renderGridItem}
+            keyExtractor={(item) => item.date}
+            numColumns={2}
+            showsVerticalScrollIndicator={false}
+          />
+        )
+      default:
+        return (
+          <FlatList
+            data={data}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.date}
+            showsVerticalScrollIndicator={false}
+          />
+        )
+    }
+  }
+
   return (
     <View
       style={{
@@ -104,13 +191,13 @@ const StatisticsScreen = () => {
         backgroundColor: theme === "dark" ? colors.gray : colors.white,
       }}
     >
-      {data.length > 0 && (
-        <FlatList
-          data={data}
-          renderItem={renderItem}
-          keyExtractor={(item) => item.date}
-        />
-      )}
+      <TabView
+        renderTabBar={() => <></>}
+        navigationState={{ index, routes }}
+        renderScene={renderScene}
+        onIndexChange={setIndex}
+        initialLayout={{ width: layout.width }}
+      />
       {modal && (
         <HalfScreenModal>
           <View style={{ flexDirection: "row", gap: 20 }}>
